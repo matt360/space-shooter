fdcddea8cf9b283c15b72eaf725a87059d56a71e
unknown <1502616@MC10661.uad.ac.uk>
2016-01-28 17:21:45 +0000

    fire

--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -12,7 +12,21 @@ public class PlayerController : MonoBehaviour
 	public float speed;
 	public float tilt;
 	public Boundary boundary;
+	
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
 
+	private float nextFire;
+
+	void Update ()
+	{
+		if (Input.GetButton("Fire1") && Time.time > nextFire) 
+		{
+			nextFire = Time.time + fireRate; //GameObject clone = 
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation); // as GameObject;
+		}
+	}
 
 	void FixedUpdate ()
 	{
@@ -21,16 +35,15 @@ public class PlayerController : MonoBehaviour
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-		GetComponent<Rigidbody> ().velocity = movement * speed;
+		GetComponent<Rigidbody>().velocity = movement * speed;
 
-		GetComponent<Rigidbody> ().position = new Vector3 
+		GetComponent<Rigidbody>().position = new Vector3 
 		(
-			Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+				Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
 			0.0f,
-			Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+				Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
 		);
 
-		GetComponent<Rigidbody> ().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
 	}
-
 }
